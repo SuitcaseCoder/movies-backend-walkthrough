22b2abd76d6879c8047648a6b07a8b90c4ab8391
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-08-05 10:00:40 -0500

    doPost

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 
@@ -22,6 +23,41 @@ public class MovieServlet extends HttpServlet {
             String movieString = new Gson().toJson(movie);
             out.println(movieString);
         } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        response.setContentType(("application/json"));
+        
+        try{
+//        get the reader off the request
+//            get reader returns a BufferedReader object
+//            BufferedReader is an existing class
+//            BufferedReader:a class which simplifies reading text from a character input stream. It buffers the characters in order to enable efficient reading of text data.
+//            reading a character or a byte
+            BufferedReader reader = request.getReader();
+            
+//            Gson().fromJson...
+            Movie[] movies = new Gson().fromJson(reader, Movie[].class);
+            
+            // loop over movies array and sout out every part of the movie to be printed out just to see that it came through
+            for(Movie movie: movies){
+                System.out.println(movie.getId());
+                System.out.println(movie.getTitle());
+                System.out.println(movie.getActors());
+                System.out.println(movie.getDirector());
+                System.out.println(movie.getGenre());
+                System.out.println(movie.getPoster());
+                System.out.println(movie.getImdbID());
+                System.out.println(movie.getPlot());
+                System.out.println("*************************");
+    
+    
+            }
+            
+        }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
